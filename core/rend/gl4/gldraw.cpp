5c3505d76ba6124408b42d4c39627d2a0ce99352
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2023-06-12 01:10:05 -0400

    Update gldraw.cpp

--- core/rend/gl4/gldraw.cpp
+++ core/rend/gl4/gldraw.cpp
@@ -18,8 +18,8 @@ GLuint depthSaveTexId;
 
 extern float gunx_ratio;
 extern float guny_ratio;
-extern int gunx_offset;
-extern int guny_offset;
+extern float gunx_offset;
+extern float guny_offset;
 
 static gl4PipelineShader *gl4GetProgram(bool cp_AlphaTest, bool pp_InsideClipping,
 							bool pp_Texture, bool pp_UseAlpha, bool pp_IgnoreTexA, u32 pp_ShadInstr, bool pp_Offset,
@@ -886,8 +886,8 @@ void gl4DrawGunCrosshair(u8 port)
 	float w=LIGHTGUN_CROSSHAIR_SIZE;
 	float h=LIGHTGUN_CROSSHAIR_SIZE;
 
-	x = lightgun_params[port].x - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
-	y = lightgun_params[port].y - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
+	x = (lightgun_params[port].x - (gunx_offset / 100.f * 640.f)) / gunx_ratio - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
+	y = (lightgun_params[port].y - (guny_offset / 100.f * 480.f)) / guny_ratio - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
 
 	if (lightgun_params[port].dirty || lightgunTextureId[port] == 0)
 		gl4UpdateLightGunTexture(port);
