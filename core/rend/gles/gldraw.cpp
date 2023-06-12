51f5f0bb5706236fa480f6ce81f9fa0e11ef2b3b
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2023-06-12 01:03:37 -0400

    Update gldraw.cpp

--- core/rend/gles/gldraw.cpp
+++ core/rend/gles/gldraw.cpp
@@ -856,8 +856,8 @@ void DrawGunCrosshair(u8 port)
 	float w=LIGHTGUN_CROSSHAIR_SIZE;
 	float h=LIGHTGUN_CROSSHAIR_SIZE;
 
-	x = gunx_ratio * (lightgun_params[port].x + (gunx_offset / 100 * 640)) - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
-	y = guny_ratio * (lightgun_params[port].y + (guny_offset / 100 * 480)) - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
+	x = lightgun_params[port].x - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
+	y = lightgun_params[port].y - ( LIGHTGUN_CROSSHAIR_SIZE / 2 );
 
 	if ( lightgun_params[port].dirty || lightgunTextureId[port] == 0)
 		UpdateLightGunTexture(port);
